4f118b776e713558a62396c0b95c02c9fe51a766
angelcuevas <angel.p.cuevas@gmail.com>
2024-05-02 22:43:00 -0300

    artillery example

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const addLogger = require('./middlewares/addLogger.middleware');
 const {loggerTestsRouter} = require('./routes/loggerTest.router');
+const { performanceRouter } = require('./routes/performance.router');
 const port = 8080;
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(addLogger)
 
 /** ROUTES */
 app.use('/api/logger', loggerTestsRouter)
+app.use('/api/performance', performanceRouter)
 
 
 app.listen(port, ()=>console.log(`Up and running on port ${port}`))
